155237cb6464913046b20dabda1c1c61d29405fc
Windi Restimonika <restimonikawindi@gmail.com>
2021-12-16 18:13:39 +0700

    Update

--- src/main/java/com/example/perpustakaan/dao/BookDAO.java
+++ src/main/java/com/example/perpustakaan/dao/BookDAO.java
@@ -1,12 +1,14 @@
 package com.example.perpustakaan.dao;
 
 import com.example.perpustakaan.entity.Book;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
+@Repository
 public class BookDAO extends BaseDAO<Book> {
     @Override
     public List<Predicate> predicates(Book param, CriteriaBuilder builder, Root<Book> root, boolean isCount){
